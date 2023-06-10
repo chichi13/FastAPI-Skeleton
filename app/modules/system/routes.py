0d5d292bfe89b131d144b236834ce67040e20ffb
Antoine C <antoinechiris@gmail.com>
2023-06-11 00:39:25 +0200

    fixed: SQLAlchemy 2.0 need text("SELECT 1") to work

--- app/modules/system/routes.py
+++ app/modules/system/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import text
 
 from app.core.config import settings
 from app.core.schema import DefaultResponse
@@ -23,7 +24,7 @@ def root() -> dict:
 @router.get("/health", status_code=status.HTTP_200_OK, response_model=DefaultResponse)
 async def get_health(db: AsyncSession = Depends(get_db)) -> dict:
     try:
-        healthy = await db.execute("SELECT 1")
+        healthy = await db.execute(text("SELECT 1"))
         if healthy.scalars().first() is None:
             raise HTTPException(status_code=404, detail={"msg": "Not Healthy ❌"})
     except Exception as e:
